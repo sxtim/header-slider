ec21cc2c460f89f86ae3881c5a7a7b19ce980ba2
sxtim <sxtimx@gmail.com>
2024-01-09 16:35:06 +0300

    add mobile navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
 
 
 // import Swiper bundle with all modules installed
--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,25 @@
 function mobileNav() {
 	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+	const navBtnOpen = document.querySelector("#modalOpen");
+	const navBtnClose = document.querySelector("#modalClose")
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+	const navOverlay = document.querySelector("#mobileNavOverlay");
+	const nav = document.querySelector("#mobileNav");
+	//
+	// navBtnOpen.addEventListener('click', () => {
+	// 	console.log('click');
+	// })
+
+	navBtnOpen.onclick = toggleMobileNav;
+	navBtnClose.onclick = toggleMobileNav;
+	navOverlay.onclick = toggleMobileNav;
+
+	function toggleMobileNav () {
+		navOverlay.classList.toggle("mobile-nav-overlay--open");
+		nav.classList.toggle("mobile-nav--open");
+		document.body.classList.toggle("no-scroll");
+		console.log('click')
+	}
 }
 
 export default mobileNav;
